8add7ccaa543e1b008abd2d9806e341bdc8ba91a
Tillmann Krebs <tillmann.krebs@stud.htw-dresden.de>
2024-11-21 12:37:04 +0100

    added duration slider and ingredient input

---
+++ src/components/addMealComponents/DurationSlider.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-slider
+    v-model="time"
+    color="green"
+    markers
+    snap
+    :min="0"
+    :max="10"
+    @change="emitTime"
+  />
+</template>
+
+<script setup>
+import { ref, watch } from "vue";
+
+const props = defineProps({
+  modelValue: {
+    type: Number,
+    default: 0,
+  },
+});
+const emit = defineEmits(["update:model-value"]);
+
+const emitTime = (newValue) => {
+  emit("update:model-value", newValue);
+};
+
+const time = ref(props.modelValue);
+
+watch(
+  () => props.modelValue,
+  (newValue) => {
+    time.value = newValue;
+  }
+);
+</script>
--- src/pages/AddMealPage.vue
+++ src/pages/AddMealPage.vue
@@ -2,89 +2,50 @@
   <div class="content" id="titleSelection">
     <div class="row">
       <h4 class="label">Name</h4>
-      <q-input filled v-model="text" :style="{ minWidth: '60%' }" bg-color="white"/>
+      <q-input
+        filled
+        v-model="text"
+        :style="{ minWidth: '60%' }"
+        bg-color="white"
+      />
     </div>
   </div>
   <div class="content" id="categorySelection">
     <h4>Kategorien</h4>
-    <q-select
-      filled
-      v-model="multiple"
-      multiple
-      :options="options"
-      :style="{ minWidth: '60%' }"
-      bg-color="white"
-      @update:model-value="onSelectionChange"
-    />
-    <q-dialog v-model="isDialogOpen">
-      <q-card>
-        <q-card-section>
-          <h6>Add a new category</h6>
-          <q-input
-            v-model="newCategory"
-            label="Category name"
-            filled
-            autofocus
-          />
-        </q-card-section>
-
-        <q-card-actions>
-          <q-btn flat label="Cancel" @click="closeDialog" />
-          <q-btn flat label="Save" @click="saveNewCategory" />
-        </q-card-actions>
-      </q-card>
-    </q-dialog>
+    <CategorySelection v-model="selectedCategories" />
   </div>
   <div class="content" id="durationSelection">
     <h4>Dauer</h4>
+    <DurationSlider v-model="duration" />
   </div>
   <div class="content" id="ingredientSelection">
     <h4>Zutaten</h4>
+    <IngredientSelection v-model="ingredients" />
   </div>
+  <q-btn
+    label="Top Button"
+    color="primary"
+    class="addMealButton"
+    @click="onTopButtonClick"
+  />
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import categorieList from "../assets/exampleCategories.json"
+import { ref } from "vue";
+import CategorySelection from "../components/addMealComponents/CategorySelection.vue";
+import DurationSlider from "../components/addMealComponents/DurationSlider.vue";
+import IngredientSelection from "../components/addMealComponents/IngredientSelection.vue";
 
-const text = ref('');
-const multiple = ref([]);  
-const options = ref(categorieList.categories.map(category => category.name)); 
-options.value.push("new"); 
+const text = ref("");
+const selectedCategories = ref([]);
+const duration = ref();
+const ingredients = ref([]);
 
-
-const isDialogOpen = ref(false);  
-const newCategory = ref('');
-
-const onSelectionChange = (newValues) => {
-  if (newValues.includes('new')) {
-  const index = newValues.indexOf('new');
-  if (index > -1) {
-    newValues.splice(index, 1);  // Remove 'new' from the array
-  }
-  isDialogOpen.value = true;
-}
-};
-
-const closeDialog = () => {
-  isDialogOpen.value = false;
-  newCategory.value = ''; 
-};
-
-const saveNewCategory = () => {
-  if (newCategory.value && !options.value.includes(newCategory.value)) {
-    const index = options.value.indexOf('new');
-    if (index > -1) {
-      options.value.splice(index, 1);
-    }
-
-    options.value.push(newCategory.value);
-    multiple.value.push(newCategory.value);
-
-    options.value.push('new');
-  }
-
-  closeDialog();
+const onTopButtonClick = () => {
+  console.log("Name", text.value);
+  console.log("Kategorien", selectedCategories.value);
+  console.log("Dauer", duration.value);
+  console.log("Zutaten", ingredients.value);
 };
 </script>
 
