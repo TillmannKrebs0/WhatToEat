63c930c0932906b08fc2bd0369f6fcce8a20745b
Tillmann Krebs <tillmann.krebs@stud.htw-dresden.de>
2024-12-16 15:04:42 +0100

    start-page hinzugefügt

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,47 +1,61 @@
 <template>
-  <q-layout
-    view="hHh lpR fFf"
-    v-touch-swipe.mouse.right="handleSwipeRight"
-    v-touch-swipe.mouse.left="handleSwipeLeft"
-  >
-    <q-header elevated class="bg-primary text-white" style="height: 13%">
-      <q-toolbar class="q-pt-lg q-pb-lg">
-        <div class="absolute-center text-center">
-          <h4>{{ pageTitle }}</h4>
-        </div>
-      </q-toolbar>
-    </q-header>
+  <div>
+    <!-- Show splash screen only on initial load -->
+    <SplashScreen v-if="showSplashScreen" />
 
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-    <q-footer elevated class="bg-white text-black footer-border">
-      <q-tabs class="q-mt-none custom-tabs">
-        <q-route-tab to="/" label="Übersicht" icon="home" />
-        <q-route-tab to="/addMeal" label="Hinzufügen" icon="add_circle" />
-        <q-route-tab to="/selectMeal" label="Auswählen" icon="casino" />
-      </q-tabs>
-    </q-footer>
-  </q-layout>
+    <q-layout
+      v-if="!showSplashScreen" 
+      view="hHh lpR fFf"
+      v-touch-swipe.mouse.right="handleSwipeRight"
+      v-touch-swipe.mouse.left="handleSwipeLeft"
+    >
+      <q-header elevated class="bg-primary text-white" style="height: 13%">
+        <q-toolbar class="q-pt-lg q-pb-lg">
+          <div class="absolute-center text-center">
+            <h4>{{ pageTitle }}</h4>
+          </div>
+        </q-toolbar>
+      </q-header>
+
+      <q-page-container>
+        <router-view />
+      </q-page-container>
+
+      <q-footer elevated class="bg-white text-black footer-border">
+        <q-tabs class="q-mt-none custom-tabs">
+          <q-route-tab to="/" label="Übersicht" icon="home" />
+          <q-route-tab to="/addMeal" label="Hinzufügen" icon="add_circle" />
+          <q-route-tab to="/selectMeal" label="Auswählen" icon="casino" />
+        </q-tabs>
+      </q-footer>
+    </q-layout>
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { useRoute, useRouter } from "vue-router";
+import { ref, computed, onMounted } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import SplashScreen from '../pages/SplashScreen.vue'; 
 
-// Titles for each route
+const router = useRouter();
+const route = useRoute();
+const showSplashScreen = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    showSplashScreen.value = false;
+    router.push('/');
+  }, 1500);
+});
+
 const routeTitles = {
   "/": "Gerichte Übersicht",
   "/addMeal": "Gericht Hinzufügen",
   "/selectMeal": "Zufallsauswahl",
 };
 
-const route = useRoute();
-const router = useRouter();
-
 const pageTitle = computed(() => routeTitles[route.path] || "Default Title");
 
-// Swipe navigation logic
 const routes = ["/", "/addMeal", "/selectMeal"];
 const navigate = (direction) => {
   const currentRoute = route.path;
---
+++ src/pages/SplashScreen.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="splash-screen">
+    <img :src="logo" alt="WhatToEat Logo" class="logo" />
+    <h2>WhatToEat</h2>
+  </div>
+</template>
+
+
+
+<script setup>
+  import logo from "../../public/icons/WTElogo1.png"
+</script>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Lora:ital,wght@0,400..700;1,400..700&family=Noto+Sans:ital,wght@0,100..900;1,100..900&family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap');
+
+.splash-screen {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: white;
+  flex-direction: column;
+  text-align: center;
+}
+h2 {
+  font-family: 'Caveat', cursive;
+  font-weight: 700;
+  font-size: 5em;
+  margin-top: 1.5em;
+  color: #EA5C2B;
+  position: absolute;
+  top: 0;
+}
+
+.logo {
+  width: 70vw; /* Set the width of the image */
+  height: auto; /* Keep the aspect ratio */
+  background-color: white;
+}
+</style>
